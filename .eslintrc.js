f0ef86b28ba88b1dbe3af4e5fec1f9f1607718e0
Colin Ogoo <colin.ogoo@bynd.com>
2019-05-17 22:36:17 +0100

    style: 💄 add linting and configs
    - prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'node': true,
+        'es6': true
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
+    },
+    'rules': {
+
+    },
+    "plugins": [
+      "svelte3"
+    ],
+};
---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  printWidth: 80,
+  singleQuote: true,
+  tabWidth: 2,
+  arrowParens: 'always',
+  trailingComma: 'es5',
+};
